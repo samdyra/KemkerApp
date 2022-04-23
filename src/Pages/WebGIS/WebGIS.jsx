0e713740ff89642f16e5291d1ef717a0177b0c93
samdyra <197413vr@gmail.com>
2022-04-24 02:30:26 +0700

    progress on maps

--- src/Pages/WebGIS/WebGIS.jsx
+++ src/Pages/WebGIS/WebGIS.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import style from "./WebGIS.module.css";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const WebGIS = () => {
-  return <div>WebGIS</div>;
+  const {
+    container,
+    settingContainer,
+    legendContainer,
+    mapContainer,
+    wrapper,
+  } = style;
+  return (
+    <>
+      <div className={container}>
+        <div className={settingContainer}></div>
+        <div className={wrapper}>
+          <div className={legendContainer}></div>
+          <div className={mapContainer}>
+            <MapContainer
+              center={[-6.927348780149033, 107.7695420971685]}
+              zoom={16}
+              style={{
+                height: "100%",
+                position: "relative",
+                zIndex: 0,
+              }}
+            >
+              <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
+            </MapContainer>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WebGIS;
