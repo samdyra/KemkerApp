21288d710ca1cce511c60888386ed08f07161719
samdyra <197413vr@gmail.com>
2022-04-27 15:54:13 +0700

    fixing basemap

--- src/Constants/Basemaps/baseMaps.js
+++ src/Constants/Basemaps/baseMaps.js
@@ -1,3 +1,3 @@
 const light = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const dark = "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
+const dark = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
 export { light, dark };
