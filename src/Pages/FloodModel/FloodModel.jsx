6b88d176de61300b746c66e64d02221fef401bcf
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2022-05-29 21:42:43 +0700

    hotfix/fotoIMG

--- src/Pages/FloodModel/FloodModel.jsx
+++ src/Pages/FloodModel/FloodModel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../ComponentsV2/Navbar";
 import style from "./FloodModel.module.css";
-import fotoIMG from "../../Assets/Images/fotoIMG.jpg";
+import fotoIMG from "../../Assets/Images/logoKemker.png";
 
 const FloodModel = () => {
   const { headerWrapper, headerText, latarContainer, latarTitle, latarContent, hiasanStyle, bc } = style;
--- src/Pages/ProfileModel/ProfileMap.jsx
+++ src/Pages/ProfileModel/ProfileMap.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../ComponentsV2/Navbar";
 import style from "./ProfileMap.module.css";
-import fotoIMG from "../../Assets/Images/fotoIMG.jpg";
+import fotoIMG from "../../Assets/Images/logoKemker.png";
 
 const ProfileMap = () => {
   const { headerWrapper, headerText, latarContainer, latarTitle, latarContent, hiasanStyle, bc } = style;
