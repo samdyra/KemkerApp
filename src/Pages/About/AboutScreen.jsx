28cab2d21595cd594d2402d0dd0610423537f1e0
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2022-05-30 21:47:29 +0700

    progress on aboyutScreen

--- src/Pages/About/AboutScreen.jsx
+++ src/Pages/About/AboutScreen.jsx
@@ -1,10 +1,50 @@
 import React from "react";
-import NavBarDefault from "../../ComponentsV2/NavbarDefault";
+import Navbar from "../../ComponentsV2/Navbar";
+import style from "./AboutScreen.module.css";
 
 const AboutScreen = () => {
+  const {
+    bc,
+    headerWrapper,
+    headerText,
+    descWrapper,
+    descImage,
+    descContainer,
+    descTitle,
+    descContent,
+    madeWrapper,
+    madeTitle,
+    madeElementWrapper,
+    madeElementContainer,
+    madeElementImage,
+    madeElementTitle,
+    madeElementContent,
+    madeElementButton,
+  } = style;
   return (
-    <div>
-      <NavBarDefault></NavBarDefault>
+    <div className={bc}>
+      <Navbar></Navbar>
+      <div className={headerWrapper}>
+        <div className={headerText}>Kemah Kerja 2022</div>
+      </div>
+      <div className={descWrapper}>
+        <div className={descImage}></div>
+        <div className={descContainer}>
+          <div className={descTitle}></div>
+          <div className={descContent}></div>
+        </div>
+      </div>
+      <div className={madeWrapper}>
+        <div className={madeTitle}></div>
+        <div className={madeElementWrapper}>
+          <div className={madeElementContainer}>
+            <div className={madeElementImage}></div>
+            <div className={madeElementTitle}></div>
+            <div className={madeElementContent}></div>
+            <div className={madeElementButton}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
