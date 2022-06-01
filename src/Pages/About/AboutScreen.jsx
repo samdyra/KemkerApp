c54a547b1a4960a9a025e1300ac680cd28317dab
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2022-06-01 17:24:28 +0700

    finishing what we made

--- src/Pages/About/AboutScreen.jsx
+++ src/Pages/About/AboutScreen.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../../ComponentsV2/Navbar";
 import style from "./AboutScreen.module.css";
-
+import { noImage } from "../../Assets";
+import foto from "../../Assets/Images/fotokemker.png";
 const AboutScreen = () => {
   const {
     bc,
@@ -20,28 +21,74 @@ const AboutScreen = () => {
     madeElementTitle,
     madeElementContent,
     madeElementButton,
+    bcd,
   } = style;
   return (
     <div className={bc}>
+      <div className={bcd}>
+        <img src={foto}></img>
+      </div>
       <Navbar></Navbar>
       <div className={headerWrapper}>
         <div className={headerText}>Kemah Kerja 2022</div>
       </div>
       <div className={descWrapper}>
-        <div className={descImage}></div>
+        <div className={descImage}>
+          <img src={noImage}></img>
+        </div>
         <div className={descContainer}>
-          <div className={descTitle}></div>
-          <div className={descContent}></div>
+          <div className={descTitle}>Kemah Kerja 2022</div>
+          <div className={descContent}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+            optio natus odit sequi? Quis similique vitae quibusdam natus sequi
+            iusto corrupti aliquid enim cumque perspiciatis nisi magni ducimus
+            reprehenderit asperiores soluta maxime, dicta hic obcaecati quisquam
+            accusantium? Earum maiores voluptatibus officia voluptas labore.
+            Ducimus nulla, labore fugiat alias deserunt cupiditate.
+          </div>
         </div>
       </div>
       <div className={madeWrapper}>
-        <div className={madeTitle}></div>
+        <div className={madeTitle}>What We Made</div>
         <div className={madeElementWrapper}>
           <div className={madeElementContainer}>
-            <div className={madeElementImage}></div>
-            <div className={madeElementTitle}></div>
-            <div className={madeElementContent}></div>
-            <div className={madeElementButton}></div>
+            <div className={madeElementImage}>
+              <img src={noImage}></img>
+            </div>
+            <div className={madeElementTitle}>WebGIS</div>
+            <div className={madeElementContent}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At iure
+              ratione laudantium ducimus vel sapiente, qui ea! Earum, sit ullam!
+              Dignissimos iure quam ab dolore ipsam fugit voluptatem
+              voluptatibus quas.
+            </div>
+            <div className={madeElementButton}>View WebGIS</div>
+          </div>
+          <div className={madeElementContainer}>
+            <div className={madeElementImage}>
+              <img src={noImage}></img>
+            </div>
+            <div className={madeElementTitle}>WebGIS</div>
+            <div className={madeElementContent}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At iure
+              ratione laudantium ducimus vel sapiente, qui ea! Earum, sit ullam!
+              Dignissimos iure quam ab dolore ipsam fugit voluptatem
+              voluptatibus quas.
+            </div>
+            <div className={madeElementButton}>View WebGIS</div>
+          </div>
+          <div className={madeElementContainer}>
+            <div className={madeElementImage}>
+              <img src={noImage}></img>
+            </div>
+            <div className={madeElementTitle}>WebGIS</div>
+            <div className={madeElementContent}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. At iure
+              ratione laudantium ducimus vel sapiente, qui ea! Earum, sit ullam!
+              Dignissimos iure quam ab dolore ipsam fugit voluptatem
+              voluptatibus quas.
+            </div>
+            <div className={madeElementButton}>View WebGIS</div>
           </div>
         </div>
       </div>
--- src/Pages/ProfileModel/ProfileMap.jsx
+++ src/Pages/ProfileModel/ProfileMap.jsx
@@ -4,7 +4,15 @@ import style from "./ProfileMap.module.css";
 import fotoIMG from "../../Assets/Images/logoKemker.png";
 
 const ProfileMap = () => {
-  const { headerWrapper, headerText, latarContainer, latarTitle, latarContent, hiasanStyle, bc } = style;
+  const {
+    headerWrapper,
+    headerText,
+    latarContainer,
+    latarTitle,
+    latarContent,
+    hiasanStyle,
+    bc,
+  } = style;
   return (
     <div className={bc}>
       <Navbar></Navbar>
@@ -14,16 +22,28 @@ const ProfileMap = () => {
       <div className={latarContainer}>
         <div className={latarTitle}>Latar Belakang</div>
         <div className={latarContent}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore eveniet quia, molestiae veritatis eligendi mollitia et minima incidunt, architecto ab
-          eum quo dolorum accusantium sequi quis asperiores commodi nobis esse recusandae repudiandae aut. Dolore distinctio, quisquam nemo dicta ipsam vel!
-          Quidem totam quibusdam vel suscipit ad in error odio ullam maiores quod earum nihil, perspiciatis repudiandae magnam. Eligendi odit facere unde enim,
-          minus recusandae iusto nostrum, provident, architecto earum a id delectus tempore? Velit, harum. Nesciunt a, nihil voluptate odit ipsum sapiente eius
-          atque minus. Quibusdam recusandae, temporibus inventore modi, tempora explicabo, cum corporis sapiente repudiandae dignissimos aliquam error quidem
-          consequatur facere fugit odit. Possimus, ullam sit expedita itaque et veniam nisi odit alias dolores, culpa eaque officiis minima laboriosam
-          cupiditate, voluptas delectus ipsam! Deserunt eius aut consequuntur tempora eaque ratione, rem saepe harum et id cum laborum, distinctio dolorem
-          natus! Molestias assumenda, unde a eius sequi sapiente similique obcaecati eum quia culpa dolore earum placeat quis cumque dolor sunt maxime tempora
-          fugiat veritatis distinctio tenetur. Quidem at ab labore, dolor provident quod officiis doloremque, minus maxime quia nobis consequuntur quos ex
-          eligendi alias repellat iure laboriosam. Molestiae, dicta nisi rerum molestias numquam deleniti voluptatum quia perspiciatis totam cumque ea.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          eveniet quia, molestiae veritatis eligendi mollitia et minima
+          incidunt, architecto ab eum quo dolorum accusantium sequi quis
+          asperiores commodi nobis esse recusandae repudiandae aut. Dolore
+          distinctio, quisquam nemo dicta ipsam vel! Quidem totam quibusdam vel
+          suscipit ad in error odio ullam maiores quod earum nihil, perspiciatis
+          repudiandae magnam. Eligendi odit facere unde enim, minus recusandae
+          iusto nostrum, provident, architecto earum a id delectus tempore?
+          Velit, harum. Nesciunt a, nihil voluptate odit ipsum sapiente eius
+          atque minus. Quibusdam recusandae, temporibus inventore modi, tempora
+          explicabo, cum corporis sapiente repudiandae dignissimos aliquam error
+          quidem consequatur facere fugit odit. Possimus, ullam sit expedita
+          itaque et veniam nisi odit alias dolores, culpa eaque officiis minima
+          laboriosam cupiditate, voluptas delectus ipsam! Deserunt eius aut
+          consequuntur tempora eaque ratione, rem saepe harum et id cum laborum,
+          distinctio dolorem natus! Molestias assumenda, unde a eius sequi
+          sapiente similique obcaecati eum quia culpa dolore earum placeat quis
+          cumque dolor sunt maxime tempora fugiat veritatis distinctio tenetur.
+          Quidem at ab labore, dolor provident quod officiis doloremque, minus
+          maxime quia nobis consequuntur quos ex eligendi alias repellat iure
+          laboriosam. Molestiae, dicta nisi rerum molestias numquam deleniti
+          voluptatum quia perspiciatis totam cumque ea.
         </div>
       </div>
       <div className={hiasanStyle}>
@@ -32,16 +52,28 @@ const ProfileMap = () => {
       <div className={latarContainer}>
         <div className={latarTitle}>Analisis</div>
         <div className={latarContent}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore eveniet quia, molestiae veritatis eligendi mollitia et minima incidunt, architecto ab
-          eum quo dolorum accusantium sequi quis asperiores commodi nobis esse recusandae repudiandae aut. Dolore distinctio, quisquam nemo dicta ipsam vel!
-          Quidem totam quibusdam vel suscipit ad in error odio ullam maiores quod earum nihil, perspiciatis repudiandae magnam. Eligendi odit facere unde enim,
-          minus recusandae iusto nostrum, provident, architecto earum a id delectus tempore? Velit, harum. Nesciunt a, nihil voluptate odit ipsum sapiente eius
-          atque minus. Quibusdam recusandae, temporibus inventore modi, tempora explicabo, cum corporis sapiente repudiandae dignissimos aliquam error quidem
-          consequatur facere fugit odit. Possimus, ullam sit expedita itaque et veniam nisi odit alias dolores, culpa eaque officiis minima laboriosam
-          cupiditate, voluptas delectus ipsam! Deserunt eius aut consequuntur tempora eaque ratione, rem saepe harum et id cum laborum, distinctio dolorem
-          natus! Molestias assumenda, unde a eius sequi sapiente similique obcaecati eum quia culpa dolore earum placeat quis cumque dolor sunt maxime tempora
-          fugiat veritatis distinctio tenetur. Quidem at ab labore, dolor provident quod officiis doloremque, minus maxime quia nobis consequuntur quos ex
-          eligendi alias repellat iure laboriosam. Molestiae, dicta nisi rerum molestias numquam deleniti voluptatum quia perspiciatis totam cumque ea.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+          eveniet quia, molestiae veritatis eligendi mollitia et minima
+          incidunt, architecto ab eum quo dolorum accusantium sequi quis
+          asperiores commodi nobis esse recusandae repudiandae aut. Dolore
+          distinctio, quisquam nemo dicta ipsam vel! Quidem totam quibusdam vel
+          suscipit ad in error odio ullam maiores quod earum nihil, perspiciatis
+          repudiandae magnam. Eligendi odit facere unde enim, minus recusandae
+          iusto nostrum, provident, architecto earum a id delectus tempore?
+          Velit, harum. Nesciunt a, nihil voluptate odit ipsum sapiente eius
+          atque minus. Quibusdam recusandae, temporibus inventore modi, tempora
+          explicabo, cum corporis sapiente repudiandae dignissimos aliquam error
+          quidem consequatur facere fugit odit. Possimus, ullam sit expedita
+          itaque et veniam nisi odit alias dolores, culpa eaque officiis minima
+          laboriosam cupiditate, voluptas delectus ipsam! Deserunt eius aut
+          consequuntur tempora eaque ratione, rem saepe harum et id cum laborum,
+          distinctio dolorem natus! Molestias assumenda, unde a eius sequi
+          sapiente similique obcaecati eum quia culpa dolore earum placeat quis
+          cumque dolor sunt maxime tempora fugiat veritatis distinctio tenetur.
+          Quidem at ab labore, dolor provident quod officiis doloremque, minus
+          maxime quia nobis consequuntur quos ex eligendi alias repellat iure
+          laboriosam. Molestiae, dicta nisi rerum molestias numquam deleniti
+          voluptatum quia perspiciatis totam cumque ea.
         </div>
       </div>
     </div>
